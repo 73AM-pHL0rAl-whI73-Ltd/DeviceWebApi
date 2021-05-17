6426c60a6acc468927a75e73d6c44760ac961014
Conny Hansson <conhan@hotmail.se>
2021-05-17 16:07:40 +0200

    Added addMeasurement and getAllDeviceMeasurements methods to interface

--- src/main/java/com/example/devicewebapi/DAO/IDeviceDataAccess.java
+++
@@ -1,4 +0,0 @@
-package com.example.devicewebapi.DAO;
-
-public interface IDeviceDataAccess {
-}
---
+++ src/main/java/com/example/devicewebapi/DAO/IDeviceMeasurementDataAccess.java
@@ -0,0 +1,14 @@
+package com.example.devicewebapi.DAO;
+
+import com.example.devicewebapi.models.DeviceMessage;
+
+import java.util.List;
+
+public interface IDeviceMeasurementDataAccess {
+
+    // add measurements
+    void addDeviceMeasurement(DeviceMessage measurement);
+
+    // get all measurements
+    List<DeviceMessage> getAllDeviceMeasurements();
+}
