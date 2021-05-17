cda02c1f91932e41865545e52f6637ef1da1304b
Marcus Nilsson <brainbomb@gmail.comb>
2021-05-17 16:27:23 +0200

    DeviceApiController: Create mappings
    Create mappings for:
    deviceService.getAllMessages()
    deviceService.getMessageById()
    deviceService.deleteMessageById()

--- src/main/java/com/example/devicewebapi/controllers/DeviceApiController.java
+++ src/main/java/com/example/devicewebapi/controllers/DeviceApiController.java
@@ -1,4 +1,30 @@
 package com.example.devicewebapi.controllers;
 
+import com.example.devicewebapi.models.DeviceMessage;
+import com.example.devicewebapi.services.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("measurements")
+@RestController
 public class DeviceApiController {
+    private final DeviceService deviceService;
+
+    @Autowired
+    public DeviceApiController(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @GetMapping
+    public List<DeviceMessage> getAllMeasurements() {return deviceService.getAllMessages();}
+
+    @GetMapping(path = "/{id}")
+    public DeviceMessage getMeasurementById(@PathVariable("id") long id) {
+        return deviceService.getMessageById(id).orElse(null);
+    }
+    @DeleteMapping(path = "/{id}")
+    public void deleteMeasurementById(@PathVariable("id") long id) {deviceService.deleteMessageById(id);}
 }
