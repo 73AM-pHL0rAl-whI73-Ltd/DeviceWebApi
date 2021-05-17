ab6019314f504d2077f50ff6dabf9201bf54244f
Marcus Nilsson <brainbomb@gmail.comb>
2021-05-17 16:54:07 +0200

    DeviceApiController: Add addDeviceMeasurement mapping

--- src/main/java/com/example/devicewebapi/controllers/DeviceApiController.java
+++ src/main/java/com/example/devicewebapi/controllers/DeviceApiController.java
@@ -2,6 +2,7 @@ package com.example.devicewebapi.controllers;
 
 import com.example.devicewebapi.models.DeviceMessage;
 import com.example.devicewebapi.services.DeviceService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +18,10 @@ public class DeviceApiController {
     public DeviceApiController(DeviceService deviceService) {
         this.deviceService = deviceService;
     }
+    @PostMapping
+    public void addDeviceMeasurement(@RequestBody DeviceMessage message) {deviceService.addDeviceMeasurement(message);}
 
     @GetMapping
-    public List<DeviceMessage> getAllMeasurements() {return deviceService.getAllMessages();}
+    public List<DeviceMessage> getAllMeasurements() {return deviceService.getAllDeviceMeasurements();}
 
-    @GetMapping(path = "/{id}")
-    public DeviceMessage getMeasurementById(@PathVariable("id") long id) {
-        return deviceService.getMessageById(id).orElse(null);
     }
-    @DeleteMapping(path = "/{id}")
-    public void deleteMeasurementById(@PathVariable("id") long id) {deviceService.deleteMessageById(id);}
-}
