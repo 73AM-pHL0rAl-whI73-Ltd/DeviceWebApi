b35afd5e71c82928cd2767285709081753eba5da
Conny Hansson <conhan@hotmail.se>
2021-05-17 13:59:48 +0200

    initial commit

---
+++ src/main/java/com/example/devicewebapi/DeviceWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.devicewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devicewebapi/DeviceWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devicewebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
