d198c88668e7612b49421c7b002329093de52dce
Marcus Nilsson <brainbomb@gmail.comb>
2021-05-18 13:24:08 +0200

    Tests: Add first test

--- src/test/java/com/example/devicewebapi/DeviceWebApiApplicationTests.java
+++ src/test/java/com/example/devicewebapi/DeviceWebApiApplicationTests.java
@@ -1,17 +1,30 @@
 package com.example.devicewebapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DeviceWebApiApplicationTests {
+    @Autowired
+    MockMvc mvc;
 
     @Test
-    void contextLoads() {
-    }
+    public void Test_getAllMeasurements()
+            throws Exception {
 
-    @Test
-    void Test_getAllMeasurements(){
-
+        mvc.perform(MockMvcRequestBuilders
+                .get("/measurements")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
