2ffd2ae32bc44f0610c2a7b5766ec7916fffd6a7
Krut-Oskar <53490024+Krut-Oskar@users.noreply.github.com>
2021-05-18 15:15:28 +0200

    Device: refactored to separate functions for main functionalities and added comments

--- devices/esp32.ino
+++ devices/esp32.ino
@@ -3,23 +3,25 @@
 #include <ArduinoJson.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
-
+//Defines pin for temp sensor and creates an instance of DHT class.
 #define DHT_PIN 4
 #define DHT_TYPE DHT11
 DHT dht(DHT_PIN, DHT_TYPE);
-
+//Constant variables for timeserver, network credentials and device ID.
 const char* ntpServer = "pool.ntp.org";
 const char* ssid = "";
 const char* pw = "";
 const char* url = "https://devicewebapi.herokuapp.com/measurements";
 const char* deviceId = "Williams DHT11";
-
+//variables for delay.
 unsigned long prevMillis = 0;
-unsigned long timeDelay = 60000;
+unsigned long timeDelay = 10000;
 
 
 void setup() {
+  //Initiate serial communication.
   Serial.begin(115200);
+  //Initates Wifi.
   WiFi.begin(ssid, pw);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -27,9 +29,11 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
   Serial.println(WiFi.localIP());
+  //configure time from time server.
   configTime(0, 0, ntpServer);
 
 }
+//gets time from timeserver and returns it.
 unsigned long getTime() {
   time_t now;
   struct tm timeinfo;
@@ -41,46 +45,51 @@ unsigned long getTime() {
   return now;
 }
 
-/*void httpPost(char message[256]) {
-  
-  
-}*/
-
-/*char* createMessage(float temp, float hum, unsigned long unixTime) {
-  
+//Creates a JSON formatted String to be sent via HTTP.
+String createMessage(float temp, float hum, unsigned long unixTime) {
+  String message;
+  DynamicJsonDocument doc(1024);
+  doc["temperature"] = temp;
+  doc["humidity"] = hum;
+  doc["timeStamp"] = unixTime;
+  doc["deviceId"] = deviceId;
+  serializeJson(doc, message);
   return message;
-}*/
+}
+//Takes a JSON formatted string and posts via HTTP to the specified url.
+int httpPost(String message) {
+  HTTPClient http;
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  int response = http.POST(message);
+  http.end();
+  return response;
+}
+
 
 void loop() {
-  float temp = 15.0;//dht.readTemperature();
-  float hum  = 20.0;//dht.readHumidity();
+  //Reads values from DHT sensor and creates a timestamp.
+  float temp = 15.2;//dht.readTemperature();
+  float hum  = 20.2;//dht.readHumidity();
   unsigned long unixTime = getTime();
   if((millis() - prevMillis) > timeDelay){
-    Serial.print("Temp: ");
-    Serial.println(temp);
-    Serial.print("Hum: ");
-    Serial.println(hum);
-    Serial.print("Time: ");
-    Serial.println(unixTime);
     if (WiFi.status() == WL_CONNECTED) {
-      HTTPClient http;
-      http.begin(url);
-      http.addHeader("Content-Type", "application/json");
-      char message[256];
-      DynamicJsonDocument doc(1024);
-      doc["temperature"] = temp;
-      doc["humidity"] = hum;
-      doc["timeStamp"] = unixTime;
-      doc["deviceId"] = deviceId;
-      serializeJson(doc, message);
-      int response = http.POST(message);
-      Serial.print("HTTP response: ");
-      Serial.println(response);
-      http.end();
+      //if delay timer has passed and WiFi is connected, creates a message and sends it via HTTP, prints out send status to serial monitor.
+      String message = createMessage(temp, hum, unixTime);
+      int response = httpPost(message);
+      if ( response == 200) {
+        Serial.print("Message sent OK: ");
+        Serial.println(message);
+      }
+      else{
+        Serial.print("HTTP ERROR CODE: ");
+        Serial.println(response);
+      }
     }
     else{
       Serial.println("No WIFI");
     }
+    //updates delay timer.
     prevMillis = millis();
   }
 
