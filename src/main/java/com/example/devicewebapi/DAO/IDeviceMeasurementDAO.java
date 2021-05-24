b92e37b145a2f95ef17100081df187f6e43df9a9
Krut-Oskar <53490024+Krut-Oskar@users.noreply.github.com>
2021-05-24 15:07:36 +0200

    IDeviceMeasurementDAO: Added builder to Device class and implemented in IDeviceMeasurement

--- src/main/java/com/example/devicewebapi/DAO/IDeviceMeasurementDAO.java
+++ src/main/java/com/example/devicewebapi/DAO/IDeviceMeasurementDAO.java
@@ -3,6 +3,7 @@ package com.example.devicewebapi.DAO;
 import com.example.devicewebapi.models.Device;
 import com.example.devicewebapi.models.DeviceMessage;
 import com.example.devicewebapi.models.DhtMessage;
+import lombok.Builder;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,20 +19,14 @@ public interface IDeviceMeasurementDAO {
     List<DhtMessage> getMeasurementByDeviceId(UUID id);
 
 
-    List<DhtMessage> getLatestDeviceMeasurements(Device device, int amount);
+    List<DhtMessage> getLatestDeviceMeasurements(Device.DeviceBuilder device, int amount);
 
     default List<DhtMessage> getLatestMeasurementByAlias(String alias, int amount) {
-        // TODO : lookup builder pattern to make this look cleaner
-        var device = new Device();
-        device.setDeviceAlias(alias);
 
-        return getLatestDeviceMeasurements(device, amount);
+        return getLatestDeviceMeasurements(Device.builder().deviceAlias(alias), amount);
     }
     default List<DhtMessage> getLatestMeasurementById(UUID id, int amount) {
-        // TODO : lookup builder pattern to make this look cleaner
-        var device = new Device();
-        device.setDeviceId(id);
 
-        return getLatestDeviceMeasurements(device, amount);
+        return getLatestDeviceMeasurements(Device.builder().deviceId(id), amount);
     }
 }
--- src/main/java/com/example/devicewebapi/models/Device.java
+++ src/main/java/com/example/devicewebapi/models/Device.java
@@ -2,6 +2,7 @@ package com.example.devicewebapi.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Device {
     private @JsonProperty("deviceId") UUID deviceId;
     private @JsonProperty("deviceAlias") String deviceAlias;
