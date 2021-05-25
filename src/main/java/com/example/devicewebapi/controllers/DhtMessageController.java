9c6c42488f3d1f6ab1e64618e32285265b538e0c
Krut-Oskar <53490024+Krut-Oskar@users.noreply.github.com>
2021-05-25 15:09:39 +0200

    Renamed DeviceMeasurementService

--- src/main/java/com/example/devicewebapi/controllers/DhtMessageController.java
+++ src/main/java/com/example/devicewebapi/controllers/DhtMessageController.java
@@ -2,7 +2,7 @@ package com.example.devicewebapi.controllers;
 
 import com.example.devicewebapi.models.DhtMessage;
 import com.example.devicewebapi.services.DashboardService;
-import com.example.devicewebapi.services.DeviceMeasurementService;
+import com.example.devicewebapi.services.DhtMessageService;
 
 import com.example.devicewebapi.services.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +16,14 @@ import java.util.UUID;
 @RestController
 public class DhtMessageController {
 
-    private final DeviceMeasurementService deviceMeasurementService;
+    private final DhtMessageService dhtMessageService;
     // services webservice/dashboard
     private final DashboardService dashboardService;
     private final DeviceService deviceService;
 
     @Autowired
-    public DhtMessageController(DeviceMeasurementService deviceMeasurementService, DashboardService dashboardService, DeviceService deviceService) {
-        this.deviceMeasurementService = deviceMeasurementService;
+    public DhtMessageController(DhtMessageService dhtMessageService, DashboardService dashboardService, DeviceService deviceService) {
+        this.dhtMessageService = dhtMessageService;
         this.dashboardService = dashboardService;
         this.deviceService = deviceService;
     }
@@ -32,29 +32,29 @@ public class DhtMessageController {
         var device = deviceService.getDeviceById(message.getDeviceId());
         if(device == null) return; // do not add measurement if no device found
 
-        deviceMeasurementService.addDeviceMeasurement(message);
+        dhtMessageService.addDeviceMeasurement(message);
         dashboardService.updateDashboard(device);
     }
 
     @GetMapping
-    public List<DhtMessage> getAllMeasurements() {return deviceMeasurementService.getAllDeviceMeasurements();}
+    public List<DhtMessage> getAllMeasurements() {return dhtMessageService.getAllDeviceMeasurements();}
 
     @GetMapping("/latest")
-    public List<DhtMessage> getLatestOneMeasurements() { return deviceMeasurementService.getLatestMeasurements(1);}
+    public List<DhtMessage> getLatestOneMeasurements() { return dhtMessageService.getLatestMeasurements(1);}
 
     @GetMapping("/latest/{top}")
-    public List<DhtMessage> getLatestMeasurements(@PathVariable int top) { return deviceMeasurementService.getLatestMeasurements(top);}
+    public List<DhtMessage> getLatestMeasurements(@PathVariable int top) { return dhtMessageService.getLatestMeasurements(top);}
 
     @GetMapping("/latest/alias/{alias}/{top}")
     public List<DhtMessage> getLatestMeasurementsByAlias(@PathVariable String alias, @PathVariable int top) {
-        return deviceMeasurementService.getLatestMeasurementByAlias(alias, top);
+        return dhtMessageService.getLatestMeasurementByAlias(alias, top);
     }
     @GetMapping("/latest/id/{id}/{top}")
     public List<DhtMessage> getLatestMeasurementsById(@PathVariable UUID id, @PathVariable int top) {
-        return deviceMeasurementService.getLatestMeasurementById(id, top);
+        return dhtMessageService.getLatestMeasurementById(id, top);
     }
 
     @GetMapping("/device/{id}")
-    public List<DhtMessage> getMeasurementByDeviceId(@PathVariable UUID id) { return deviceMeasurementService.getMeasurementByDeviceId(id);}
+    public List<DhtMessage> getMeasurementByDeviceId(@PathVariable UUID id) { return dhtMessageService.getMeasurementByDeviceId(id);}
 
     }
